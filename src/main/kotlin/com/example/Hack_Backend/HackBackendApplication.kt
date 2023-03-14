27d9ae835a5015c70b895b664a3846755bc4d3f6
Edward Todd <edwardtdd@gmail.com>
2023-03-14 10:53:53 +0000

    Initial commit

---
+++ src/main/kotlin/com/example/Hack_Backend/HackBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Hack_Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HackBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<HackBackendApplication>(*args)
+}
